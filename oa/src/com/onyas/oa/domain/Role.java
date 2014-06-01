dc5d0568ad41101030b4faf1f7a57aa65f7fde80
onyas <zheng0518@163.com>
2014-06-01 15:04:02 +0800

    系统管理模块--实体类的设计

--- oa/src/com/onyas/oa/domain/Role.java
+++ oa/src/com/onyas/oa/domain/Role.java
@@ -1,9 +1,22 @@
 package com.onyas.oa.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 实体：岗位
+ * @author Administrator
+ *
+ */
 public class Role {
 
 	private Long id;
+	private Set<User> users = new HashSet<User>();
+	
 	private String name;
+	private String description;
+	
+	
 	public Long getId() {
 		return id;
 	}
@@ -16,5 +29,16 @@ public class Role {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+	public Set<User> getUsers() {
+		return users;
+	}
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
