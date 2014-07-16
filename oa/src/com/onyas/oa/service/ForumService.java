d0f5ebb8a8c65d5b48216de4b047344668ad077d
onyas <zheng0518@163.com>
2014-07-16 21:56:23 +0800

    版块管理增删改查功能的实现

--- oa/src/com/onyas/oa/service/ForumService.java
+++ oa/src/com/onyas/oa/service/ForumService.java
@@ -5,4 +5,16 @@ import com.onyas.oa.domain.Forum;
 
 public interface ForumService extends BaseDao<Forum> {
 
+	/**
+	 * 上移，当在最上面时不能上移
+	 * @param id
+	 */
+	void moveUp(long id);
+
+	/**
+	 * 下移，当在最下面是不能下移
+	 * @param id
+	 */
+	void moveDown(long id);
+
 }
--- oa/src/com/onyas/oa/service/impl/ForumServiceImpl.java
+++ oa/src/com/onyas/oa/service/impl/ForumServiceImpl.java
@@ -9,5 +9,15 @@ import com.onyas.oa.service.ForumService;
 @Service
 public class ForumServiceImpl extends BaseDaoImpl<Forum> implements ForumService {
 
+	@Override
+	public void moveDown(long id) {
+		
+	}
+
+	@Override
+	public void moveUp(long id) {
+		
+	}
+
 
 }
