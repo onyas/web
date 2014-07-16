510ed06d8cfd0d3eed75a63d99fec3df4be3e6f4
onyas <zheng0518@163.com>
2014-07-16 21:58:22 +0800

    解决添加时一直跳转到修改的问题，因为之前用的是long型的id,它的默认值为0，所以在判断是否为空时，一直都是false,所以会执行edit方法，而Long型的id默认为null，所以不会有这种情况

--- oa/src/com/onyas/oa/domain/Forum.java
+++ oa/src/com/onyas/oa/domain/Forum.java
@@ -7,15 +7,15 @@ package com.onyas.oa.domain;
  */
 public class Forum {
 
-	private long id;
+	private Long id;
 	private String name;
 	private String description;
 	private int position;
 	
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	public String getName() {
