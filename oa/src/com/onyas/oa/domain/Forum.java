eccf7cda36d4c59ef2fc4ce472b9eef507a1b60a
onyas <zheng0518@163.com>
2014-07-16 20:29:21 +0800

    版块管理功能的实体设计

---
+++ oa/src/com/onyas/oa/domain/Forum.java
@@ -0,0 +1,40 @@
+package com.onyas.oa.domain;
+
+/**
+ * 版块管理
+ * @author Administrator
+ *
+ */
+public class Forum {
+
+	private long id;
+	private String name;
+	private String description;
+	private int position;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getPosition() {
+		return position;
+	}
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	
+}
