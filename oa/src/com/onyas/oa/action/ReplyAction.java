e130b37b64042407bf8d17cdd136cbef5595691c
onyas <zheng0518@163.com>
2014-07-19 20:46:31 +0800

    发表回复

--- oa/src/com/onyas/oa/action/ReplyAction.java
+++ oa/src/com/onyas/oa/action/ReplyAction.java
@@ -1,22 +1,50 @@
 package com.onyas.oa.action;
 
+import java.util.Date;
+
+import org.apache.struts2.ServletActionContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import com.onyas.oa.base.BaseAction;
 import com.onyas.oa.domain.Reply;
+import com.onyas.oa.domain.Topic;
+import com.opensymphony.xwork2.ActionContext;
 
 @Controller
 @Scope("prototype")
 public class ReplyAction extends BaseAction<Reply> {
 
+	private Long topicId;
+	
 	/** 发表新回复页面 */
 	public String addUI() throws Exception {
+		//准备数据
+		Topic topic = topicService.getById(topicId);
+		ActionContext.getContext().put("topic", topic);
 		return "addUI";
 	}
 
 	/** 发表新回复页面 */
 	public String add() throws Exception {
+		//封装(已发封装好了title,content,faceIcon)
+		model.setTopic(topicService.getById(topicId));
+		
+		model.setAuthor(getCurrentUser());
+		model.setIpAddr(ServletActionContext.getRequest().getRemoteAddr());
+		model.setPostTime(new Date());
+		//保存
+		replyService.save(model);
+		
 		return "toTopicShow";// 转到新回复所属主题的显示页面
 	}
+
+	public Long getTopicId() {
+		return topicId;
+	}
+
+	public void setTopicId(Long topicId) {
+		this.topicId = topicId;
+	}
+	
 }
--- oa/src/com/onyas/oa/service/impl/ReplyServicImpl.java
+++ oa/src/com/onyas/oa/service/impl/ReplyServicImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.onyas.oa.base.BaseDaoImpl;
+import com.onyas.oa.domain.Forum;
 import com.onyas.oa.domain.Reply;
 import com.onyas.oa.domain.Topic;
 import com.onyas.oa.service.ReplyService;
@@ -21,4 +22,22 @@ public class ReplyServicImpl extends BaseDaoImpl<Reply> implements ReplyService{
 		.list();
 	}
 
+	@Override
+	public void save(Reply reply) {
+		//保存到DB
+		getSession().save(reply);
+		//维护相关的信息
+		Topic topic = reply.getTopic();
+		Forum forum = topic.getForum();
+		
+		forum.setArticleCount(forum.getArticleCount()+1);//版块的文章数（主题加回复）
+		topic.setReplyCount(topic.getReplyCount()+1);//主题的回复数
+		topic.setLastReply(reply);//主题的最后发表的回复
+		topic.setLastUpdateTime(reply.getPostTime());//主题的最后更新的时间(主题的发表时间或是最后回复时间)
+		
+		
+		getSession().update(topic);
+		getSession().update(forum);
+	}
+	
 }
