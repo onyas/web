0124802ba00814e2350dd9de87b3225877df0083
onyas <zheng0518@163.com>
2014-07-19 13:57:05 +0800

    解决置顶贴，精华贴，普通贴排序的问题

--- oa/src/com/onyas/oa/service/impl/TopicServicImpl.java
+++ oa/src/com/onyas/oa/service/impl/TopicServicImpl.java
@@ -17,7 +17,7 @@ public class TopicServicImpl extends BaseDaoImpl<Topic> implements TopicService 
 	public List<Topic> findByForum(Forum forum) {
 		return getSession()
 		//TODO:排序:所有置顶贴都在最上面，然后把最新状态的主题显示到前面
-		.createQuery("FROM Topic t WHERE t.forum=? ORDER BY t.type DESC,t.lastUpdateTime DESC")
+		.createQuery("FROM Topic t WHERE t.forum=? ORDER BY (CASE t.type WHEN 2 THEN 2 ELSE 0 END ) DESC,t.lastUpdateTime DESC")
 		.setParameter(0, forum)
 		.list();
 	}
