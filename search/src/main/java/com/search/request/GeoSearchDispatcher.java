3369c3df66b25c455fe3922ac3e16657b3c7498d
zhenglinlin <zhenglinlin@haodehaode.cn>
2015-09-09 15:57:38 +0800

    fix bugs

--- search/src/main/java/com/search/request/GeoSearchDispatcher.java
+++ search/src/main/java/com/search/request/GeoSearchDispatcher.java
@@ -1,12 +1,15 @@
 package com.search.request;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.search.common.Constant;
 import com.search.protocol.GeoIndexDO;
 import com.search.service.SolrEngine;
 
 public class GeoSearchDispatcher implements Dispatcher {
+	private static final Logger log = LoggerFactory
+			.getLogger(GeoSearchDispatcher.class);
 	String q = "q=*:*&wt=json&start=0&rows=1000&fq={!geofilt}&pt=%s&sfield=store&d=0.5&sort=geodist()%%20asc&fl=distance:geodist(),*";// 搜索附近的人，通过fl同时返回距离
 
 	private SolrEngine solrEngine;
@@ -14,13 +17,13 @@ public class GeoSearchDispatcher implements Dispatcher {
 	public Object process(Object message) {
 		GeoIndexDO geoIndex = (GeoIndexDO) message;
 		String lanlng = geoIndex.getStore();
-		q = String.format(q, lanlng);
-		return solrEngine.search(Constant.GEOCORENAME, q);
+		String param = String.format(q, lanlng);
+		log.info("GeoSearchDispatcher param::{}",param);
+		return solrEngine.search(Constant.GEOCORENAME, param);
 	}
 
 	public void setSolrEngine(SolrEngine solrEngine) {
 		this.solrEngine = solrEngine;
 	}
-	
-	
+
 }
